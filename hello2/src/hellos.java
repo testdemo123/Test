a7ac5232c650aa36bfb9aab746577519422df32a
testdemo123 <sh.rafatnia@gmail.com>
2013-01-29 18:57:36 -0700

    I've made a new branch for adding new features!

--- hello2/src/hellos.java
+++ hello2/src/hellos.java
@@ -8,6 +8,8 @@ public class hellos {
 		// TODO Auto-generated method stub
 		System.out.println("test");
 		System.out.println("test from Chenlei");
+		System.out.println("This a new branch");
+		
 		
 	}
 
